12662c0c24b4b839c773eee44eaa38d8de4620f0
vitaliiex <151929039+vitaliiex@users.noreply.github.com>
2023-12-19 01:03:45 +0300

    Update Program.c

--- singlestone/Program.cs
+++ singlestone/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace singlestone
+﻿namespace Dz5_Singletone
 {
+    public sealed class Singleton
+    {
+
+        private Singleton() { }
+
+        private static Singleton MarkValue;
+
+
+        public static Singleton GetInstance()
+        {
+            if (MarkValue == null)
+            {
+                MarkValue = new Singleton();
+            }
+            return MarkValue;
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Все працюне");
+            }
+            else
+            {
+                Console.WriteLine("Не працює");
+            }
         }
     }
 }
